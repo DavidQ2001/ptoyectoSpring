0eda2342662b23e440cbd1d39858db9ac396a74d
MILLERxJ <contactomillerxj@gmail.com>
2023-05-23 08:40:35 -0500

    first commit

---
+++ src/main/java/com/example/pruebapersonal/servicios/ServicioAutor.java
@@ -0,0 +1,94 @@
+package com.example.pruebapersonal.servicios;
+
+import com.example.pruebapersonal.entidades.Autor;
+import com.example.pruebapersonal.repositorios.AutorRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ServicioAutor implements ServicioBase<Autor>{
+
+    @Autowired
+    private AutorRepositorio autorRepositorio;
+
+    @Override
+    public List<Autor> buscarTodos() throws Exception {
+        try{
+            List<Autor>autores=autorRepositorio.findAll();
+            return autores;
+
+        }catch (Exception error){
+            throw new Exception(error.getMessage());
+
+        }
+    }
+
+    @Override
+    public Autor buscarId(Integer id) throws Exception {
+        try{
+           Optional<Autor> autorOptional =  autorRepositorio.findById(id);
+           if(autorOptional.isPresent()){
+               Autor autor = autorOptional.get();
+               return autor;
+
+           }else{
+               throw new Exception("El autor buscado no existe" +id);
+           }
+
+        }catch (Exception error){
+            throw new Exception(error.getMessage());
+
+        }
+    }
+
+    @Override
+    public Autor registrar(Autor datosEntidad) throws Exception {
+        try{
+            Autor autorGuardado=autorRepositorio.save(datosEntidad);
+            return autorGuardado;
+
+        }catch (Exception error){
+            throw new Exception(error.getMessage());
+
+        }
+    }
+
+    @Override
+    public Autor actualizar(Integer id, Autor datosEntidadNuevos) throws Exception {
+        try{
+            Optional<Autor> autorOptional=autorRepositorio.findById(id);
+            if(autorOptional.isPresent()){
+                //filtrar datos a actualizar
+                Autor autorExistente = autorOptional.get();
+                autorExistente.setNombre(datosEntidadNuevos.getNombre());
+                Autor autorActualizado=autorRepositorio.save(autorExistente);
+                return autorActualizado;
+            }else{
+                throw new Exception("El autor buscado no existe" +id);
+            }
+        }catch (Exception error){
+            throw new Exception(error.getMessage());
+
+        }
+    }
+
+    @Override
+    public boolean eliminar(Integer id) throws Exception {
+        try{
+            Optional<Autor> autorOptional=autorRepositorio.findById(id);
+            if(autorOptional.isPresent()){
+                autorRepositorio.deleteById(id);
+                return true;
+            }else{
+                throw new Exception("El autor buscado no existe" +id);
+            }
+
+        }catch (Exception error){
+
+            throw new Exception(error.getMessage());
+        }
+    }
+}
